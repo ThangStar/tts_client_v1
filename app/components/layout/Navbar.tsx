da0e3b7b6115dc17a4cbf8664ab0f93651fbe9b8
Thắng Văn <nguyenquocthinhit@gmail.com>
2025-08-31 16:44:34 +0700

    main

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="h-16 px-4 border-b bg-white/50 backdrop-blur-xl">
+    <nav className="h-16 px-4 border-b bg-white/50 backdrop-blur-xl ">
       <div className="h-full flex items-center justify-between relative">
         <Link href="/" passHref className="flex items-center gap-2">
           <AudioWaveform width={32} height={32} className="text-primary" />

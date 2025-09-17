0ca56dd5fd0d59767a065f839c25b450cc4e8d17
Thắng Văn <nguyenquocthinhit@gmail.com>
2025-09-17 11:15:44 +0700

    main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'VIVOICE',
+      name: 'web_tts',
       script: 'npm',
       args: 'start',
+      cwd: './',
       env: {
         NODE_ENV: 'production',
-        PORT: 3030
+        PORT: 3000
       },
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G'
+      max_memory_restart: '1G',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
     }
   ]
 } 
